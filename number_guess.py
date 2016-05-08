1cc5d156c0c6a23dd4b8a0530529f7c511a56d5d
NavedCoded <navecoded@gmail.com>
2016-05-08 22:22:20 +0100

    It works!!!

--- number_guess.py
+++ number_guess.py
@@ -1,12 +1,11 @@
 print('Hello!')
-print('Please choose a number \(in your head\) between 1 and 10, and not including them.')
+print('Please choose a number (in your head) between 1 and 10, and not including them.')
 
-response = input('Is your number divisible by 2 \(y or n\)?')
+response1 = input('Is your number divisible by 2 (y or n)?')
 
-if input = 'y':
+if response1 == 'y':
     # 2, 4, 6, 8 are possible
     print('yes!')
-
-if input = 'n':
+if response1 == 'n':
     # 3, 5, 7, 9 are possible
-    print
+    print('no!')
