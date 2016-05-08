efa2101e85f4c2d4f84443ce18d40900d0742fbc
NavedCoded <navecoded@gmail.com>
2016-05-08 22:28:01 +0100

    Number of imporvements: else, and small improvements

--- number_guess.py
+++ number_guess.py
@@ -6,6 +6,11 @@ response1 = input('Is your number divisible by 2 (y or n)?')
 if response1 == 'y':
     # 2, 4, 6, 8 are possible
     print('yes!')
-if response1 == 'n':
+
+elif response1 == 'n':
     # 3, 5, 7, 9 are possible
-    print('no!')
+    response2 = input('')
+
+else:
+    print('Please restart the script, and answer either y or n')
+
