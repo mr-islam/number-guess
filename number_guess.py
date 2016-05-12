3feb0c23513ab5afba63b2bb54c1bef9c58e3da2
NavedCoded <navecoded@gmail.com>
2016-05-12 14:37:58 +0100

    Introduced user prompt going to new line.

--- number_guess.py
+++ number_guess.py
@@ -3,17 +3,17 @@ import time
 print('Hello!')
 time.sleep(1)  # delays for 2 seconds
 
-print('Please choose an integer (in your head) between 1 and 10, but not including them.')
+print('Please choose an integer (in your head) between 1 and 10, but not including them.\n')
 time.sleep(2)
 
-response1 = input('Is your number divisible by 2 (y or n)?')
+response1 = input('Is your number divisible by 2 (y or n)?\n')
 
 if response1 == 'y':
     # 2, 4, 6, 8 are possible
-    response2 = input('Is your number divisible by 4 (y or n)?')
+    response2 = input('Is your number divisible by 4 (y or n)?\n')
     if response2 == 'y':
         # 4, 8 are possible
-        response3 = input('Is the square root of your number 2 (y or n)?')
+        response3 = input('Is the square root of your number 2 (y or n)?\n')
         if response3 == 'y':
             print('Your number is 4!')
         elif response3 == 'n':
@@ -22,20 +22,20 @@ if response1 == 'y':
             print('Please answer only y or n')
     elif response2 == 'n':
         # 2, 6 are possible
-        response3 = input('Is your number prime (y or n)?')
+        response3 = input('Is your number prime (y or n)?\n')
         if response3 == 'y':
             print('Your number is 2!')
         if response3 == 'n':
             print('Your number is 6!')
     else:
-        print('Please restart the script answering only y or n')
+        print('Please restart the script answering only y or n\n')
 
 elif response1 == 'n':
     # 3, 5, 7, 9 are possible
-    response2 = input('Is your number divisible by 3 (y or n)')
+    response2 = input('Is your number divisible by 3 (y or n)\n')
     if response2 == 'y':
         # 3, 9 are possible
-        response3 = input('Is your number prime (y or n)?')
+        response3 = input('Is your number prime (y or n)?\n')
         if response3 == 'y':
             print('Your number is 3!')
         if response3 == 'n':
@@ -43,7 +43,7 @@ elif response1 == 'n':
 
     elif response2 == 'n':
         # 5, 7 are possible
-        response3 = input('Is your number divisible by 5 (y or n)?')
+        response3 = input('Is your number divisible by 5 (y or n)?\n')
         if response3 == 'y':
             print('Your number is 5!')
         if response3 == 'n':
